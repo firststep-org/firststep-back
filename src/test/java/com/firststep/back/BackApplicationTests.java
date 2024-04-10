eb42fce3f59e356b7eaacbede5bfdb740d5b33d1
peppone-choi <peppone.choi@gmail.com>
2024-04-10 20:15:15 +0900

    init

---
+++ src/test/java/com/firststep/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firststep.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
