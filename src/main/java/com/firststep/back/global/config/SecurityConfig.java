b865b44534bec23fe3dd3900f125f84114d2b4b4
peppone-choi <peppone.choi@gmail.com>
2024-04-22 17:04:02 +0900

    feat: JWT Filter 구현

--- src/main/java/com/firststep/back/global/config/SecurityConfig.java
+++ src/main/java/com/firststep/back/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.firststep.back.global.config;
 
+import com.firststep.back.global.config.security.JwtFilter;
+import com.firststep.back.global.config.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
+    private final JwtTokenProvider tokenProvider;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -39,6 +42,7 @@ public class SecurityConfig {
                                     .permitAll();
                         }
                 )
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .headers(
                         headers ->
                                 headers.frameOptions(
--- src/main/java/com/firststep/back/global/config/security/JwtFilter.java
+++ src/main/java/com/firststep/back/global/config/security/JwtFilter.java
@@ -1,20 +1,40 @@
 package com.firststep.back.global.config.security;
 
-
+import io.jsonwebtoken.lang.Strings;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        log.info("인증 시도");
+        String token = getToken(request);
+        if (Strings.hasText(token) && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
 
+    private String getToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            return token;
+        }
+        return null;
     }
 }
